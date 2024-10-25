d1e183068ca0abd6c08f94df37ce710749bed6fc
merginn <murathan.egin@gmail.com>
2024-10-25 15:51:27 +0300

    make it better2

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def admin():
 def admin_panel():
     return render_template('admin_panel.html', students=students)
 
+@app.route('/delete_student/<int:student_id>', methods=['POST'])
+def delete_student(student_id):
+    global students
+    # Delete the student by filtering out the student with the matching ID
+    students = [student for student in students if student['id'] != student_id]
+    return redirect(url_for('admin_panel'))
+
 @app.route('/student', methods=['GET', 'POST'])
 def student():
     if request.method == 'POST':
@@ -33,8 +40,9 @@ def student():
         group_id = request.form['group_id']
         subject = request.form['subject']
 
-        # Save the details, including the subject
+        # Save the details, including an ID
         new_student = {
+            'id': len(students) + 1,  # Simple ID generation; consider UUID for production
             'first_name': first_name,
             'surname': surname,
             'group_id': group_id,
@@ -47,22 +55,6 @@ def student():
 @app.route('/student_panel')
 def student_panel():
     return render_template('student_panel.html')
-
-@app.route('/submit_student_details', methods=['POST'])
-def submit_student_details():
-    first_name = request.form['first_name']
-    surname = request.form['surname']
-    group_id = request.form['group_id']
-    subject = request.form['subject']
-
-    new_student = {
-        'first_name': first_name,
-        'surname': surname,
-        'group_id': group_id,
-        'subject': subject
-    }
-    students.append(new_student)
-    return redirect(url_for('student_panel'))
 
 if __name__ == '__main__':
     app.run(debug=True)
